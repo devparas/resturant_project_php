a24ca49feaa1be6fd27152c1a4ed1c8852683621
devparas <90160271+devparas@users.noreply.github.com>
2021-09-06 05:53:50 +0545

    Add files via upload

---
+++ pratice-5-resturant/config/constants.php
@@ -0,0 +1,19 @@
+<?php
+    //session start here
+    session_start();
+    define('HOMEURL','http://localhost/pratice-5-resturant/');
+
+    // creating the constant
+
+    define('LOCALHOST','localhost');
+    define('DB_USERNAME','root');
+    define('DB_PASSWORD','');
+    define('DB_NAME','food-order');
+
+    $con = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die(mysqli_error());//db connection
+    $db_select = mysqli_select_db($con,DB_NAME) or die(mysqli_error());// selecting db
+
+
+
+?>
+
